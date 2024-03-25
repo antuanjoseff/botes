11f54b28b50014684eb030c523c70ebc379a7975
antuanjoseff <toni.hernandez@udg.edu>
2024-03-25 08:11:07 +0100

    Customize map widget

--- kms/admin.py
+++ kms/admin.py
@@ -4,6 +4,8 @@ from django.db.models import Sum, Max
 from django.db.models import Subquery
 from django.contrib.gis import admin as gisadmin
 from django.contrib.gis.forms.widgets import OSMWidget
+from django.contrib.gis.forms.widgets import OpenLayersWidget
+from django.contrib.gis.forms.widgets import BaseGeometryWidget
 
 
 class CustomBotesFilter(admin.SimpleListFilter):
@@ -48,8 +50,42 @@ class BotesAdmin(admin.ModelAdmin):
     last_date.admin_order_field = 'last_date'
 
 
-class CustomGeoWidget(OSMWidget):
+class CustomGeoWidget(BaseGeometryWidget):
     template_name = 'gis/custom_layers.html'
+    map_srid = 3857
+    default_lon = 3
+    default_lat = 41.95
+    default_zoom = 10
+    
+    class Media:
+        css = {
+            "all": (
+                "https://cdn.jsdelivr.net/npm/ol@v7.2.2/ol.css",
+                "gis/css/ol3.css",
+            )
+        }
+        js = (
+            "https://cdn.jsdelivr.net/npm/ol@v7.2.2/dist/ol.js",
+            "gis/js/OLMapWidget_custom.js",
+        )
+
+    def serialize(self, value):
+        return value.json if value else ""
+
+    def deserialize(self, value):
+        geom = super().deserialize(value)
+        # GeoJSON assumes WGS84 (4326). Use the map's SRID instead.
+        if geom and json_regex.match(value) and self.map_srid != 4326:
+            geom.srid = self.map_srid
+        return geom
+
+    def __init__(self, attrs=None):
+        super().__init__()
+        for key in ("default_lon", "default_lat", "default_zoom"):
+            self.attrs[key] = getattr(self, key)
+            
+        if attrs:
+            self.attrs.update(attrs)
 
 
 class KmsAdmin(gisadmin.GISModelAdmin):
@@ -57,19 +93,13 @@ class KmsAdmin(gisadmin.GISModelAdmin):
     gis_widget = CustomGeoWidget
     ordering = ('-date', 'distancia', 'ruta')
     list_display = ("ruta", "date", "distancia","gpx_file",)
-    gis_widget_kwargs = {
-        'attrs': {
-            'default_zoom': 14,
-            'default_lon': 3.4825,
-            'default_lat': 50.1906,
-        },
-    }
+    
 
     class Meta:
         widgets = {
             'geom': CustomGeoWidget
         }
-
+    
     def get_readonly_fields(self, request, obj=None):
         return []
 
